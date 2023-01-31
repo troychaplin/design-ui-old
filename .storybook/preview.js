2f897812c87f65540a81bf6b063947707eb88f18
Troy Chaplin <troy.chaplin@gmail.com>
2023-01-31 17:44:16 -0500

    Updated build tools

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,15 @@
 import '../src/styles/index.css'
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    options: {
+        storySort: {
+            order: ['Get Started', ['Home', 'Single'], 'Components'],
+        },
     },
-  },
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
+    },
 }
